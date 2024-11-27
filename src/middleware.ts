7257cd21b2b39953bfc3bf6c8306b9ce04239629
Priyangsu Banik <priyangsubanik2003@gmail.com>
2024-11-27 17:30:53 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,26 +5,27 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("session")?.value;
 
+  // If session cookie exists, do a redirect to the homepage
   if (cookie !== undefined) {
     try {
+      // Create a response and delete the cookie
       const customNextResponse = NextResponse.redirect(
         new URL("/", request.nextUrl)
       );
-      customNextResponse.cookies.delete("session");
-
-      return customNextResponse;
+      customNextResponse.cookies.delete("session"); // Delete session cookie
+      return customNextResponse; // Return the response with the deleted cookie
     } catch (error) {
       console.log(error);
+      // Redirect to login if an error occurs
       return NextResponse.redirect(new URL("/auth/login", request.nextUrl));
     }
   } else {
+    // If the session cookie is not found, redirect to login
     return NextResponse.redirect(new URL("/auth/login", request.nextUrl));
   }
-
-  NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
+// Define which paths this middleware will apply to
 export const config = {
-  matcher: ["/", "/profile"],
+  matcher: ["/", "/profile"], // Apply middleware to these paths
 };
